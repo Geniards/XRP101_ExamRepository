82384928a8b9092f251ed38daed526c6e5cf2fa8
Geniards <geniard93@gmail.com>
2024-11-19 13:50:52 +0900

    [Refactor] Q3_터렛 범위를 벗어날시 총알 발사를 멈추게 수정.

--- Q_03/Assets/Scripts/TurretController.cs
+++ Q_03/Assets/Scripts/TurretController.cs
@@ -19,9 +19,22 @@ public class TurretController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("범위 안에 들어옴.");
         if (other.CompareTag("Player"))
         {
             Fire(other.transform);
+        }
+    }
+
+    // Q3_4.발사 범위를 벗어나도 계속 플레이어를 타겟을 잡아서 발사하는 문제 발생.
+    // 해결방안) 범위를 벗어날시 코루틴을 꺼서 발사하는 부분을 멈추게한다.
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StopCoroutine(_coroutine);
+            _coroutine = null;
+            Debug.Log("플레이어 범위에서 벗어남. 사격 중지.");
         }
     }
 
